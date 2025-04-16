9f1f0f9f8f605cf5ed27ba986b22bcda41751f04
Di <diana8burkut@gmail.com>
2025-04-16 19:25:55 +0200

    base svg display block

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,9 @@
-export default function Product() {
- return (
-<div>
-<h2>Cookies</h2>
-	  <p>Price: 999 credits</p>
-</div>
- );
+export default function Product({name, imgUrl, price}) {
+  return (
+   <div>
+      <h2>{name}</h2>
+	  <img src={imgUrl} alt={name} width="480" />
+	  <p>Price: {price} credits</p>
+    </div>
+  );
 };
